d4a33ee2ea3aa8af89afa44e54d92de4f7e7e253
Daniela Alvarez Martinez <112136245+danielaAm-hash@users.noreply.github.com>
2023-11-05 16:12:48 -0600

    Add files via upload

---
+++ Comandos basicos en R.R
@@ -0,0 +1,58 @@
+#Comandos basicos en R 
+
+#Ejemplo 1 
+print("hello")
+
+#Ejemplo 2 
+mean(c(1,5,6,2,3,4))
+
+#Ejemplo 3
+a <- c(1,7,8,10,25)
+print(mean(a))
+
+#Ejemplo 4
+sd(a)
+
+#Ejemplo 5 
+print("hello")
+a <- c(1,7,8,10,25)
+mean(a)
+
+#Ejemplo 6 
+(3 + exp(2)) / sin(pi)
+
+#Ejemplo 7 
+1+2*3
+1 + (2*3)
+(1 + 2) * 3
+
+#Ejemplo 8 
+1 + 1
+2 * 2
+2 + 10 / 5
+(2 + 10) / 5
+10^2 + 1
+sqrt(9)
+pi
+print(pi, digits = 22)
+sin(pi)
+log(100)
+log10(100)
+log2(8)
+exp(1)
+
+#Ejemplo 9
+a <- 1
+a + 1
+a
+b <- 10
+b <- a + b
+b
+3e-2 * 2.0
+
+#Ejemplo 10 
+b <- LETTERS[1:10]
+b[-1]
+b[-1.1]
+b[-1.9999]
+b[-2]
